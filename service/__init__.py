0959b7e62ab063dabb84e07a39b3b00b3fc726ed
IsaacOnKeys <iamnoticandy@protonmail.com>
2024-12-10 11:42:31 -0500

    fix other pipline errors

--- service/__init__.py
+++ service/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 # pylint: disable=wrong-import-position,cyclic-import
 # This must be imported after the Flask app is created
-from service import routes 
+from service import routes
 from service.common import log_handlers
 
 log_handlers.init_logging(app, "gunicorn.error")
